d89db4574c43a87cecf212a209264acc0467e28d
SameekshaAKotian09 <sameekshakotian0@gmail.com>
2026-08-12 12:24:25 +0530

    Fix auth middleware filename case

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Task = require("../models/task");
-const protect = require("../middleware/authMiddleware");
+const protect = require("../middleware/authmiddleware");
 
 const router = express.Router();
 
